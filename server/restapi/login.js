c3dde45cf5ee2d042641a103439ecb2992f8b8b9
Shogo Furusawa <graighle@gmail.com>
2018-09-16 18:45:43 +0900

    ログインAPIをエラーレスポンスクラスに対応．

--- server/restapi/login.js
+++ server/restapi/login.js
@@ -1,33 +1,41 @@
+import ErrorResponse from '../lib/ErrorResponse';
 import { Collections, collection } from '../lib/mongo';
 
 export default function login(req, res, next){
 	try{
-		if(!req.is('application/json')){
-			res.sendStatus(400);
-			return;
-		}
+		if(!req.is('application/json'))
+			throw new ErrorResponse({status:400});
 
 		// usersコレクションにid/passwordがあればtokenを生成して返す
 		// TODO:パスワードハッシュ化
 		collection(Collections.USERS)
 			.findOne({id:req.body.id, password:req.body.password})
-			.then(function(doc){
-				if(doc != null){
-					res.send({
-						accessToken: 'token-sample'
-					});
-				}else{
-					res.sendStatus(401);
-				}
+			.then(doc => {
+				if(doc === null)
+					throw new ErrorResponse({status:401});
+				return doc;
 			})
-			.catch(function(err){
-				console.log('[api/login] ' + err.name + ' : ' + err.message);
-				next(err);
+			.then(doc => {
+				res.send({
+					accessToken: 'token-sample'
+				});
+			})
+			.catch(err => {
+				if('status' in err){
+					res.sendStatus(err.status);
+				}else{
+					console.log('[api/login] ' + err.name + ' : ' + err.message);
+					next(err);
+				}
 			});
 	}
 	catch(err){
-		console.log('[api/login] ' + err.name + ' : ' + err.message);
-		next(err);
+		if('status' in err){
+			res.sendStatus(err.status);
+		}else{
+			console.log('[api/login] ' + err.name + ' : ' + err.message);
+			next(err);
+		}
 	}
 }
 
