c536bab958346d5ef143280169689a1047ea8420
Shogo Furusawa <graighle@gmail.com>
2018-09-16 14:11:16 +0900

    ログインAPIをGETで実装．

--- server/restapi/login.js
+++ server/restapi/login.js
@@ -1,8 +1,32 @@
-//import { Collections } from '../lib/mongo';
+import { Collections, collection } from '../lib/mongo';
 
 export default function login(req, res){
-	res.send({
-		accessToken	: 'token-sample'
-	});
+	try{
+		// usersコレクションにid/passwordがあればtokenを生成して返す
+		// TODO:パスワードハッシュ化
+		collection(Collections.USERS)
+			.findOne({id:req.query.id, password:req.query.password})
+			.then(function(doc){
+				if(doc != null){
+					res.send({
+						accessToken: 'token-sample'
+					});
+				}else{
+					res.sendStatus(401);
+				}
+			},
+			function(err){
+				console.log('[api/login] ' + err.name + ' : ' + err.message);
+				res.sendStatus(500);
+			})
+			.catch(function(err){
+				console.log('[api/login] ' + err.name + ' : ' + err.message);
+				res.sendStatus(500);
+			});
+	}
+	catch(err){
+		console.log('[api/login] ' + err.name + ' : ' + err.message);
+		res.sendStatus(500);
+	}
 }
 
