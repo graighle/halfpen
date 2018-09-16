b2a171197ccd02e3ad84828abd1812c4284411bd
Shogo Furusawa <graighle@gmail.com>
2018-09-16 14:09:41 +0900

    MongoDB接続用の関数実装．

---
+++ server/lib/mongo.js
@@ -0,0 +1,27 @@
+import { MongoClient } from 'mongodb';
+
+const url = 'mongodb://localhost:27017';
+const dbName = 'halfpen';
+
+export const Collections = {
+	USERS: 'users',
+};
+
+var db = null;
+
+MongoClient.connect(url, {useNewUrlParser:true}, function(err, client){
+	if(err != null)
+		console.log('[mongo] ' + err.name + ' : ' + err.message);
+	else
+		db = client.db(dbName);
+});
+
+export function collection(name){
+
+	if(db === null)
+		throw new Error('[mongo] Not connected.');
+
+	return db.collection(name);
+
+}
+
