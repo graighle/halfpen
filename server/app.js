bd1d00c9691c15948940ae86b6b1569050318bfb
Shogo Furusawa <graighle@gmail.com>
2018-09-23 15:26:27 +0900

    サーバでCORSを許可．

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import path from 'path';
 import logger from 'morgan';
 import config from 'config';
+import cors from 'cors';
 import restapi from './routes/restapi';
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(cors());
 app.use('/api', restapi);
 
 module.exports = app;
