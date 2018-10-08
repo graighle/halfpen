9394c162b23b0244f6c6af524a2c8fd17563cc2d
Shogo Furusawa <graighle@gmail.com>
2018-10-08 09:45:24 +0900

    ETagを無効化．

--- server/app.js
+++ server/app.js
@@ -13,6 +13,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('etag', false);
 
 app.use(cors());
 app.use('/api', restapi);
